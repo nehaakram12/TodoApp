ee8f318dcbd0bc17fce1a9399cf402652409dffc
Neha Akram <nehaakram12@gmail.com.com>
2020-06-20 22:50:57 +0500

    login api created with firebase authentication email password

--- functions/index.js
+++ functions/index.js
@@ -3,6 +3,8 @@
 const functions = require('firebase-functions');
 const app = require('express')();
 
+// ----- todo routes ----
+
 const {
 	getAllTodos,
 	postOneTodo,
@@ -14,5 +16,12 @@ app.get('/todos', getAllTodos);
 app.post('/todo', postOneTodo);
 app.delete('/todo/:todoId', deleteTodo);
 app.put('/todo/:todoId', editTodo);
+
+// ----- user routes ----
+
+const { loginUser } = require('./APIs/users');
+
+// Users
+app.post('/login', loginUser);
 
 exports.api = functions.https.onRequest(app);
---
+++ functions/util/config.js
@@ -0,0 +1,12 @@
+// config.js
+
+module.exports = {
+	apiKey: 'AIzaSyA0MB52SNEfD5kO5njayHT256AKwMDi4Jk',
+	authDomain: 'todoapp-29438.firebaseapp.com',
+	databaseURL: 'https://todoapp-29438.firebaseio.com',
+	projectId: 'todoapp-29438',
+	storageBucket: 'todoapp-29438.appspot.com',
+	messagingSenderId: '837782526614',
+	appId: '1:837782526614:web:15b69ef72e244e115840b7',
+	measurementId: 'G-94LPZMLEL9'
+};
