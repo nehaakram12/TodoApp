904cb5e49e1bad3881bd2ac7e3a574607be37e98
Neha Akram <nehaakram12@gmail.com.com>
2020-06-21 02:23:05 +0500

    Allow cors on firebase apis

--- functions/index.js
+++ functions/index.js
@@ -3,6 +3,8 @@
 const functions = require('firebase-functions');
 const app = require('express')();
 const auth = require('./util/auth');
+var cors = require('cors');
+app.use(cors({ origin: true, credentials: true }));
 
 // ----- todo routes ----
 
